77ede96475944dbfd6fe2eaec867786d08a7ecf5
regisGRB <regis.grumberg@gmail.com>
2019-11-26 10:34:54 +0100

    Entite produit
    Creation entite produit

---
+++ src/Entity/Produit.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProduitRepository")
+ */
+class Produit
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $id_salle;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date_arrivee;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date_depart;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $prix;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $etat;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdSalle(): ?int
+    {
+        return $this->id_salle;
+    }
+
+    public function setIdSalle(int $id_salle): self
+    {
+        $this->id_salle = $id_salle;
+
+        return $this;
+    }
+
+    public function getDateArrivee(): ?\DateTimeInterface
+    {
+        return $this->date_arrivee;
+    }
+
+    public function setDateArrivee(\DateTimeInterface $date_arrivee): self
+    {
+        $this->date_arrivee = $date_arrivee;
+
+        return $this;
+    }
+
+    public function getDateDepart(): ?\DateTimeInterface
+    {
+        return $this->date_depart;
+    }
+
+    public function setDateDepart(\DateTimeInterface $date_depart): self
+    {
+        $this->date_depart = $date_depart;
+
+        return $this;
+    }
+
+    public function getPrix(): ?int
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(int $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getEtat(): ?string
+    {
+        return $this->etat;
+    }
+
+    public function setEtat(string $etat): self
+    {
+        $this->etat = $etat;
+
+        return $this;
+    }
+}
