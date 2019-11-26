0cff1bf44b2e63194decf31c6aa853167bdfd148
regisGRB <regis.grumberg@gmail.com>
2019-11-26 10:42:13 +0100

    Entite Avis
    Creation entite avis

---
+++ src/Entity/Avis.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\AvisRepository")
+ */
+class Avis
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $id_membre;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $id_salle;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $commentaire;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $note;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date_enregistrement;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdMembre(): ?int
+    {
+        return $this->id_membre;
+    }
+
+    public function setIdMembre(int $id_membre): self
+    {
+        $this->id_membre = $id_membre;
+
+        return $this;
+    }
+
+    public function getIdSalle(): ?int
+    {
+        return $this->id_salle;
+    }
+
+    public function setIdSalle(int $id_salle): self
+    {
+        $this->id_salle = $id_salle;
+
+        return $this;
+    }
+
+    public function getCommentaire(): ?string
+    {
+        return $this->commentaire;
+    }
+
+    public function setCommentaire(?string $commentaire): self
+    {
+        $this->commentaire = $commentaire;
+
+        return $this;
+    }
+
+    public function getNote(): ?int
+    {
+        return $this->note;
+    }
+
+    public function setNote(?int $note): self
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    public function getDateEnregistrement(): ?\DateTimeInterface
+    {
+        return $this->date_enregistrement;
+    }
+
+    public function setDateEnregistrement(\DateTimeInterface $date_enregistrement): self
+    {
+        $this->date_enregistrement = $date_enregistrement;
+
+        return $this;
+    }
+}
