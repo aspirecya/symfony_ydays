cccf6d62c3570a5b1e505c3341b1ea265f4797a1
regisGRB <regis.grumberg@gmail.com>
2020-01-20 19:56:45 +0100

    Controller : Avis/Membre

--- src/Controller/MembreController.php
+++ src/Controller/MembreController.php
@@ -1,10 +1,11 @@
 <?php
 
 namespace App\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Entity\Membre;
+use App\Form\MembreType;
+use Symfony\Component\HttpFoundation\Request;
 class MembreController extends AbstractController
 {
     /**
@@ -15,5 +16,28 @@ class MembreController extends AbstractController
         return $this->render('membre/index.html.twig', [
             'controller_name' => 'MembreController',
         ]);
+    }
+
+
+    /**
+     * @Route("/membre/create", name="membre_create")
+     */
+    public function MembreCreate(Request $request){
+
+
+        $Membre = new Membre;
+      
+                $form =$this ->createForm(MembreType::class,$Membre);
+                $form ->handleRequest($request);
+    
+                if($form ->isSubmitted() && $form ->isValid()){
+                    $manager = $this ->getDoctrine() ->getManager();
+                    $manager ->persist($Membre);
+                    $manager -> flush();
+                    $this ->addFLash('sucess','Votre compte a bien ete creer');
+                    $this->redirectToRoute('home');
+                }
+                return $this -> render('membre/membre_form.html.twig',array('userForm' =>$form ->createView()));
+            
     }
 }
