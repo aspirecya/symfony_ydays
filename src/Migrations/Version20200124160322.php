abc035235f4d1b2068a216b6add0c63259516e02
Wassim <x.liquid1@gmail.com>
2020-01-24 17:31:41 +0100

    fix missing migrations

---
+++ src/Migrations/Version20200124160322.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200124160322 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE produit ADD CONSTRAINT FK_29A5EC27DC304035 FOREIGN KEY (salle_id) REFERENCES salle (id)');
+        $this->addSql('CREATE INDEX IDX_29A5EC27DC304035 ON produit (salle_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE produit DROP FOREIGN KEY FK_29A5EC27DC304035');
+        $this->addSql('DROP INDEX IDX_29A5EC27DC304035 ON produit');
+    }
+}
